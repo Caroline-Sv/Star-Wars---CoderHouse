c9d0bef157db3a4e79c70d02e8c264ccc0c965f1
Caroline da Silva <caroline.svs@hotmail.com>
2024-02-20 13:16:11 -0300

    ajustando js em botoes

--- scripts/personagens.js
+++ scripts/personagens.js
@@ -1,10 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("botaoForca").style.backgroundColor = "gray";
+});
 
 function mostrarPersonagensForca() {
     document.getElementById('personagensForca').style.display = 'table';
     document.getElementById('personagensSombrios').style.display = 'none';
+    document.getElementById("botaoForca").style.backgroundColor = "gray";
+    document.getElementById("botaoSombrio").style.backgroundColor = "black";
 }
 
 function mostrarPersonagensSombrio() {
     document.getElementById('personagensForca').style.display = 'none';
     document.getElementById('personagensSombrios').style.display = 'table';
+    document.getElementById("botaoForca").style.backgroundColor = "black";
+    document.getElementById("botaoSombrio").style.backgroundColor = "gray";
 }
